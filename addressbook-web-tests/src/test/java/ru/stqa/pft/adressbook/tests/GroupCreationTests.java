49406e450a744f0f843cc5480e891a97da24e33a
Evgeniy1102 <s.evgeniy1102@gmail.com>
2017-02-28 20:15:37 +0300

    Значения по умолчанию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreationTests() {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test123", "123", "123"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test123", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
